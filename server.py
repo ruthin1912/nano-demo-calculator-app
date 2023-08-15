ce402c786be8cc88454130452aa1be7a3df45f41
Ruthin Gupta <ruthinimmadisetty@gmail.com>
2023-08-15 22:47:01 +0530

    ruthin commit

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return 'Hello world'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
